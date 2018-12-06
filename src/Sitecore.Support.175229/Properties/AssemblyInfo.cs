78c509cc6feb2b98df8d8d3ce2919a06d4f7f8ce
RomanChernyk <roc@sitecore.net>
2018-12-06 14:12:47 +0200

    Initial commit

---
+++ src/Sitecore.Support.175229/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.175229")]
+[assembly: AssemblyProduct("Sitecore.Support.175229")]
+[assembly: ComVisible(false)]
